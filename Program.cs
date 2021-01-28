b3c3d0a02c31ec7b2368d4b3efab81f20c2f681b
franciscamiguel <miguelfrancisca92@gmail.com>
2021-01-28 12:46:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace idademediade5alunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal media;
+            decimal idade1;
+            decimal idade2;
+            decimal idade3;
+            decimal idade4;
+            decimal idade5;
+            Console.WriteLine("programa calcule a idade media de 5 alunos");
+            Console.Write("informe a idade do 1° aluno:");
+            idade1 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("informe a idade do 2° aluno :");
+            idade2 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("informe a idade do 3° aluno :");
+            idade3 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("informe a idade do 4° aluno :");
+            idade4 = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("informe a idade do 5° aluno :");
+            idade5 = Convert.ToDecimal(Console.ReadLine());
+            media = (idade1 + idade2 + idade3 + idade4 + idade5)/ 5;
+            Console.WriteLine(" A idade média dos alunos é " + media);
+            Console.ReadKey();
+        }
+    }
+}
